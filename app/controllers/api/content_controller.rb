c6dab203d17d69a3f9141ba4483d5d6e69478557
Junaid <junaid.ehsan12@gmail.com>
2019-02-05 16:18:52 +0500

    Api end point added for json contents object

--- app/controllers/api/content_controller.rb
+++ app/controllers/api/content_controller.rb
@@ -1,2 +1,8 @@
 class Api::ContentController < ApplicationController
+	def index
+		@contents = Content.select(:title, :published_date, :author, :summary, :content)
+		respond_to do |format|
+    	format.json  { render json: @contents } 
+  	end
+	end
 end
